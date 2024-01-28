c8bd7e9bf1372f9502d38c08c2decac58d86c6ae
James Fox <jameswfox16@gmail.com>
2024-01-28 16:59:43 -0600

    Added TodoList and AddTask base screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import AddTask from './screens/AddTask';
+import TodoList from './screens/TodoList';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='TodoList'>
+          <Stack.Screen
+            name="TodoList"
+            component={TodoList}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="AddTask"
+            component={AddTask}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/AddTask/AddTask.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const AddTask = () => {
+  return (
+    <View>
+      <Text>Add Task</Text>
+    </View>
+  );
+}
+
+export default AddTask;
